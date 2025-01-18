e3f7226105466f8d15dd5c6c89ceef33c7c053b3
JairPalaguachi <jpalagua@espol.edu.ec>
2025-01-17 20:22:20 -0500

    Update BebidaDecorator.java

--- Tarea03_EnVivoTickets/src/main/java/Decorator/BebidaDecorator.java
+++ Tarea03_EnVivoTickets/src/main/java/Decorator/BebidaDecorator.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Decorator;
 
-/**
- *
- * @author USUARIO
- */
-public class BebidaDecorator {
+import AbstractFactory.Ticket;
+
+
+public class BebidaDecorator extends Decorator {
     
+    public BebidaDecorator(Ticket ticket) {
+        super(ticket);
+    }
+
+    @Override
+    public void agregarCaracteristica() {
+        super.agregarCaracteristica();
+        System.out.println("Bebida añadida al ticket.");
+    }
 }
