8b8119eba132af6893bc988f8919f604f1b58d34
JairPalaguachi <jpalagua@espol.edu.ec>
2025-01-17 20:23:57 -0500

    Update TicketConEstacionamiento.java

--- Tarea03_EnVivoTickets/src/main/java/Decorator/TicketConEstacionamiento.java
+++ Tarea03_EnVivoTickets/src/main/java/Decorator/TicketConEstacionamiento.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Decorator;
 
-/**
- *
- * @author USUARIO
- */
-public class TicketConEstacionamiento {
-    
+import AbstractFactory.Ticket;
+
+
+public class TicketConEstacionamiento extends Ticket{
+    public TicketConEstacionamiento(Ticket ticket) {
+        super();
+        System.out.println("Configuramos el estacionamiento");
+    }
+
+    @Override
+    public void agregarCaracteristica() {
+        super.agregarCaracteristica();
+        System.out.println("Estacionamiento añadido al ticket.");
+    }
 }
