56e0b2af2ee8c06c068ca2a6a807cb316751fd71
JairPalaguachi <jpalagua@espol.edu.ec>
2025-01-17 20:22:01 -0500

    Update AlimentoDecorator.java

--- Tarea03_EnVivoTickets/src/main/java/Decorator/AlimentoDecorator.java
+++ Tarea03_EnVivoTickets/src/main/java/Decorator/AlimentoDecorator.java
@@ -1,13 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Decorator;
 
-/**
- *
- * @author USUARIO
- */
-public class AlimentoDecorator {
+import AbstractFactory.Ticket;
+
+public class AlimentoDecorator extends Decorator {
     
+    public AlimentoDecorator(Ticket ticket) {
+        super(ticket);
+    }
+
+    @Override
+    public void agregarCaracteristica() {
+        super.agregarCaracteristica();
+        System.out.println("Alimento añadido al ticket.");
+    }
 }
